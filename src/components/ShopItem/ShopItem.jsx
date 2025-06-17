56805dffa6fe7c45f04b3eb4f154dcaf1be4816a
norkitorki <norki@tuta.io>
2025-06-17 08:40:38 +0200

    Create ShopItem component

---
+++ src/components/ShopItem/ShopItem.jsx
@@ -0,0 +1,17 @@
+import styles from './ShopItem.module.css';
+import { useContext } from 'react';
+import { useParams } from 'react-router';
+import { ShopItemsContext } from '../../contexts/ShopItemsContext';
+import ShopItemCard from '../ShopItemCard/ShopItemCard';
+
+export default function ShopItem() {
+  const shopItems = useContext(ShopItemsContext);
+  const { id } = useParams();
+  const item = shopItems.find((ite) => ite.id === Number(id));
+
+  return (
+    <div className={styles.wrapper}>
+      <ShopItemCard item={item} redirectToCart={true} />
+    </div>
+  );
+}
