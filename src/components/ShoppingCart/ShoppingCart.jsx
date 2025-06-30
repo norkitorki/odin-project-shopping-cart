07ab9db517f7c6a073bad1e2dfbe532e8057d689
norkitorki <norki@tuta.io>
2025-06-30 08:05:10 +0200

    Make shopping cart compatible with narrow viewports

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -2,7 +2,56 @@ import styles from './ShoppingCart.module.css';
 import { useContext } from 'react';
 import { CartItemsContext } from '../../contexts/CartItemsContext';
 import { Link } from 'react-router';
-import { XCircle } from 'feather-icons-react';
+import { XCircle, Minus, Plus } from 'feather-icons-react';
+
+const ItemLink = ({ item }) => (
+  <Link to={`/shop/item/${item.id}`} className={styles.itemLink}>
+    <img src={item.image} className={styles.itemImage} />
+    <span>{item.title}</span>
+  </Link>
+);
+
+const ItemQuantity = ({ item, updateQuantity }) => {
+  const changeQuantity = (id, quantity) => () => updateQuantity(id, quantity);
+
+  return (
+    <div className={styles.quantityWrapper}>
+      <button
+        className={styles.quantitySelector}
+        onClick={changeQuantity(item.id, item.quantity - 1)}
+      >
+        <Minus />
+      </button>
+      <input
+        disabled
+        type="text"
+        value={item.quantity}
+        min={0}
+        className={styles.quantityDisplay}
+      />
+      <button
+        className={styles.quantitySelector}
+        onClick={changeQuantity(item.id, item.quantity + 1)}
+      >
+        <Plus />
+      </button>
+    </div>
+  );
+};
+
+const RemoveItemButton = ({ item, removeFromCart }) => {
+  const removeItem = () => removeFromCart(item.id);
+
+  return (
+    <button
+      onClick={removeItem}
+      className={styles.removeButton}
+      title={`Remove '${item.title}' from cart`}
+    >
+      <XCircle />
+    </button>
+  );
+};
 
 export default function ShoppingCart() {
   const { cartItems, total, updateQuantity, removeFromCart, clearItems } =
@@ -35,29 +84,14 @@ export default function ShoppingCart() {
           {cartItems.map((item) => (
             <tr key={item.id}>
               <td>
-                <Link to={`/shop/item/${item.id}`} className={styles.itemLink}>
-                  <img src={item.image} className={styles.itemImage} />
-                  <span>{item.title}</span>
-                </Link>
+                <ItemLink item={item} />
               </td>
               <td>
-                <input
-                  type="number"
-                  value={item.quantity}
-                  min={0}
-                  onChange={(e) => updateQuantity(item.id, e.target.value)}
-                  className={styles.quantity}
-                />
+                <ItemQuantity item={item} updateQuantity={updateQuantity} />
               </td>
-              <td>${(item.quantity * item.price).toFixed(2)}</td>
+              <td>${item.price.toFixed(2)}</td>
               <td>
-                <button
-                  onClick={() => removeFromCart(item.id)}
-                  className={styles.removeButton}
-                  title={`Remove '${item.title}' from cart`}
-                >
-                  <XCircle />
-                </button>
+                <RemoveItemButton item={item} removeFromCart={removeFromCart} />
               </td>
             </tr>
           ))}
@@ -66,22 +100,63 @@ export default function ShoppingCart() {
             <th>Total</th>
           </tr>
           <tr>
-            <td colSpan={2}>
-              <button
-                onClick={clearCart}
-                title="Clear cart items"
-                className={styles.clearButton}
-              >
-                Clear Cart
-              </button>
-              <button onClick={checkout} className={styles.checkoutButton}>
-                To Checkout
-              </button>
-            </td>
+            <td colSpan={2} />
             <td>${total.toFixed(2)}</td>
           </tr>
         </tbody>
       </table>
+
+      <table className={`${styles.itemTable} ${styles.narrowItemTable}`}>
+        {cartItems.map((item) => (
+          <tbody key={item.id}>
+            <tr>
+              <td colSpan={2}>
+                <ItemLink item={item} />
+              </td>
+            </tr>
+            <tr>
+              <th>Quantity</th>
+              <td>
+                <ItemQuantity item={item} updateQuantity={updateQuantity} />
+              </td>
+            </tr>
+            <tr>
+              <th>Price</th>
+              <td>${item.price.toFixed(2)}</td>
+            </tr>
+            <tr className={styles.lastRow}>
+              <td colSpan={2}>
+                <button
+                  onClick={() => removeFromCart(item.id)}
+                  className={styles.narrowRemoveButton}
+                  title={`Remove '${item.title}' from cart`}
+                >
+                  Remove
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        ))}
+        <tfoot>
+          <tr>
+            <th>Total</th>
+            <td>${total.toFixed(2)}</td>
+          </tr>
+        </tfoot>
+      </table>
+
+      <div className={styles.actionButtons}>
+        <button
+          onClick={clearCart}
+          className={styles.clearButton}
+          title="Clear cart items"
+        >
+          Clear Cart
+        </button>
+        <button onClick={checkout} className={styles.checkoutButton}>
+          To Checkout
+        </button>
+      </div>
     </>
   );
 }
