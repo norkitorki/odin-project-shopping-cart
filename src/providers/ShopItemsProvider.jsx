6ae224a29452f0b6b3590bfb09062f122f928e3c
norkitorki <norki@tuta.io>
2025-07-10 11:12:05 +0200

    Wrap sortItems() and filterItems() in useCallback()

--- src/providers/ShopItemsProvider.jsx
+++ src/providers/ShopItemsProvider.jsx
@@ -1,24 +1,27 @@
 import shopItems from '../assets/fakeShopItems';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { ShopItemsContext } from '../contexts/ShopItemsContext';
 
 export default function ShopItemsProvider({ children }) {
   const [filteredItems, setFilteredItems] = useState(null);
 
-  const sortItems = (property, direction) => {
-    setFilteredItems(
-      (filteredItems || shopItems).toSorted((a, b) => {
-        if (direction === 'desc') [a, b] = [b, a];
-        return a[property] <= b[property] ? -1 : 1;
-      })
-    );
-  };
+  const sortItems = useCallback(
+    (property, direction) => {
+      setFilteredItems(
+        (filteredItems || shopItems).toSorted((a, b) => {
+          if (direction === 'desc') [a, b] = [b, a];
+          return a[property] <= b[property] ? -1 : 1;
+        })
+      );
+    },
+    [filteredItems]
+  );
 
-  const filterItems = (category) => {
+  const filterItems = useCallback((category) => {
     if (category === 'default') return setFilteredItems(null);
 
     setFilteredItems(shopItems.filter((item) => item.category === category));
-  };
+  }, []);
 
   return (
     <ShopItemsContext
