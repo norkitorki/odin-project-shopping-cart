3ae88335fe3f939de2633b67c43b9fdc497f0448
norkitorki <norki@tuta.io>
2025-07-11 13:40:46 +0200

    Extend dropdown on click and remove unnecessary state from CartDropdown component

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -1,52 +1,86 @@
 import styles from './CartDropdown.module.css';
-import { useContext, useState } from 'react';
+import { useCallback, useContext, useEffect, useRef } from 'react';
 import { Link, useLocation } from 'react-router';
 import { CartItemsContext } from '../../contexts/CartItemsContext';
 import { ShoppingCart as ShoppingCartIcon } from 'feather-icons-react';
 
-const BLACKLISTED_PATHS = ['/', '/cart'];
+export default function CartDropdown({ excludedPaths = [] }) {
+  const { cartItems, addCallback, total } = useContext(CartItemsContext);
+  const location = useLocation();
+  const trigger = useRef(null);
+  const dropdown = useRef(null);
+  const timeout = useRef(null);
 
-export default function CartDropdown() {
+  const extend = useCallback(() => {
+    dropdown.current.classList.add(styles.menuExtended);
+    trigger.current.setAttribute('aria-expanded', true);
+  }, []);
 
-  if (BLACKLISTED_PATHS.includes(location.pathname)) return;
+  const retract = useCallback(() => {
+    dropdown.current.classList.remove(styles.menuExtended);
+    trigger.current.setAttribute('aria-expanded', false);
+  }, []);
+
+  const toggleDropdown = useCallback(() => {
+    clearTimeout(timeout.current);
+    dropdown.current.classList.contains(styles.menuExtended)
+      ? retract()
+      : extend();
+  }, [extend, retract]);
+
+  useEffect(() => {
+    addCallback('openDropdown', (type) => {
+      if (type !== 'add') return;
+
+      clearTimeout(timeout.current);
+      timeout.current = setTimeout(retract, 5000);
+      extend();
+    });
+    return () => clearTimeout(timeout.current);
+  }, [addCallback, extend, retract]);
+
+  if (excludedPaths.includes(location.pathname)) return;
 
   const itemCount = cartItems.length;
-  const expand = () => itemCount > 0 && setExpanded(true);
-  const retract = () => setExpanded(false);
-  const navigateToCart = (event) =>
-    event.target.nodeName !== 'A' && navigate('/cart') && retract();
 
   return (
-    <button
-      className={styles.trigger}
-      title={itemCount <= 0 ? 'Your Cart is empty' : null}
-      aria-expanded={expanded}
-      onClick={navigateToCart}
-      onMouseEnter={expand}
-      onMouseLeave={retract}
-      onFocus={expand}
-      onBlur={retract}
-    >
-      <ShoppingCartIcon strokeWidth="2.5" fill={itemCount > 0 ? 'green' : ''} />
-      <ul
-        className={`${styles.dropdown} ${expanded && styles.menuExtended}`}
-        onClick={retract}
+    <>
+      <button
+        ref={trigger}
+        className={`${styles.cartTrigger} ${
+          (itemCount > 0 && styles.cartFilled) || ''
+        }`}
+        title={
+          itemCount <= 0 ? 'Your Cart is empty' : `${itemCount} items in cart`
+        }
+        aria-expanded={false}
+        onClick={toggleDropdown}
       >
-        <li>
-          {itemCount} Item{itemCount !== 1 && 's'}
-        </li>
-        {cartItems.map((item) => (
-          <li key={item.id}>
-            <Link to={`/shop/item/${item.id}`} className={styles.itemLink}>
-              <img src={item.image} />
-              {item.title}
-            </Link>{' '}
-            x {item.quantity}
-            <hr width="90%" />
-          </li>
-        ))}
-        <li>{total.toFixed(2)}$</li>
-      </ul>
-    </button>
+        <ShoppingCartIcon className={styles.cartIcon} />
+      </button>
+      <div ref={dropdown} className={styles.dropdown} data-testid={'dropdown'}>
+        <Link to={'/cart'} className={styles.cartLink}>
+          Go To Cart
+        </Link>
+        <ul>
+          {cartItems.slice(0, 5).map((item) => (
+            <li key={item.id}>
+              <Link
+                to={`/shop/item/${item.id}`}
+                className={styles.itemLink}
+                onClick={retract}
+              >
+                <img src={item.image} />
+                {item.title}
+              </Link>{' '}
+              x {item.quantity}
+              <hr width="90%" />
+            </li>
+          ))}
+        </ul>
+        <p>{itemCount > 5 && `+${itemCount - 5} more items`}</p>
+        <p className={styles.total}>${total().toFixed(2)}</p>
+      </div>
+    </>
   );
 }
