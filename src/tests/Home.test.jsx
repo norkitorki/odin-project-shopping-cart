ee52ead66c9158f157cfec0c3ac6c3d130b3926b
norkitorki <norki@tuta.io>
2025-07-13 10:56:14 +0200

    Test for shop link presence

--- src/tests/Home.test.jsx
+++ src/tests/Home.test.jsx
@@ -1,13 +1,32 @@
 import Home from '../components/Home/Home';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
 import { test, expect } from 'vitest';
 
 test('renders heading', () => {
-  render(<Home />);
+  render(
+    <MemoryRouter>
+      <Home />
+    </MemoryRouter>
+  );
 
   expect(
     screen.getByRole('heading', {
       name: /welcome to my super awesome shop!/i,
+    })
+  ).toBeInTheDocument();
+});
+
+test('renders shop link', () => {
+  render(
+    <MemoryRouter>
+      <Home />
+    </MemoryRouter>
+  );
+
+  expect(
+    screen.getByRole('link', {
+      name: /go to shop/i,
     })
   ).toBeInTheDocument();
 });
